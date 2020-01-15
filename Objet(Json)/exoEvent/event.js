0aa51b22d26d06b74387985856f3151ed849d67c
Freek <frederik.fit@gmail.com>
2020-01-16 00:03:00 +0100

    maison

--- Objet(Json)/exoEvent/event.js
+++ Objet(Json)/exoEvent/event.js
@@ -1,15 +1,20 @@
-
 function init() {
-   // var btn1 = document.getElementById("bouton1");
-    //btn1.addEventListener("click", function() {
+  var btn1 = document.getElementById("bouton1"); //btn1 = bouton1
+  var btn2 = document.createElement("button"); // Créer un élément <button> = btn2 (mais pas encore inseré)
+  var btn_all = document.querySelectorAll("button"); //Selectionner tout les boutons (btn1+btn2 ici)
 
-
-        var btn2 = document.createElement("button");                     // Créer un élément <button>
-        var t = document.createTextNode("Bouton2");                      // Créer un noeud textuel
-        btn2.appendChild(t);                                             // Ajouter le texte au bouton
-        //var x = document.querySelectorAll("div")[1];
-        //console.log(x);
-       
-        document.body.appendChild("btn2");    // Ajoute la balise <button> à la balise <body>
-
-    } window.onload = init();
+  btn_all.forEach(function(button) {                           //pour chaqu'un des boutons 1&2 on va verifier s'il y a un evenement           
+    btn1.addEventListener("click", function() {
+      if (document.querySelectorAll("button").length == "1") {     // condition de creation du bouton 2 , seulement s'il n'existe pas
+        var t = document.createTextNode("Bouton2 (v1)");    //crée texte bouton 2
+        btn2.appendChild(t); // remplit le bouton avec le texte
+        document.getElementById("div1").appendChild(btn2); // insère le bouton 2  dans le 1er bloc
+      }
+      document.getElementById("div2").innerHTML = "mon bouton fonctionne";
+    });
+    btn2.addEventListener("click", function() {
+      document.getElementById("div2").innerHTML = "mon bouton 2";
+    });
+  });
+}
+window.onload = init;
